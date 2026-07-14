9479814e436886360d122d7f29e9fa20ca6312bc
timeflyer2005 <yanjerry2015@gmail.com>
2026-07-14 11:30:57 -0400

    Add: lru-cache - submission-5

---
+++ Data Structures & Algorithms/lru-cache/submission-5.py
@@ -0,0 +1,61 @@
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}
+
+        self.left = Node(0, 0)
+        self.right = Node(0, 0)
+
+        self.left.next = self.right
+        self.right.prev = self.left
+    
+    def remove(self, node):
+        # remove node from its current positions
+
+        prev_node = node.prev
+        next_node = node.next
+
+        prev_node.next = next_node
+        next_node.prev = prev_node 
+    def insert(self, node):
+        
+        previous_most_recent = self.right.prev
+
+        previous_most_recent.next = node
+        node.prev = previous_most_recent
+
+        node.next = self.right
+        self.right.prev = node 
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        node = self.cache[key]
+
+        self.remove(node)
+        self.insert(node)
+
+        return node.value
+
+    def put(self, key: int, value: int) -> None:
+
+        if key in self.cache:
+            self.remove(self.cache[key])
+
+        new_node = Node(key, value)
+        self.cache[key] = new_node
+        self.insert(new_node)
+
+        if len(self.cache) > self.capacity:
+            least_recent = self.left.next
+
+            self.remove(least_recent)
+            del self.cache[least_recent.key]
