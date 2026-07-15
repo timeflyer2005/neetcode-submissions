95426c59e64171cf80bcae7b58061ab4f096d90d
timeflyer2005 <yanjerry2015@gmail.com>
2026-07-15 16:43:50 -0400

    Add: binary-tree-diameter - submission-6

---
+++ Data Structures & Algorithms/binary-tree-diameter/submission-6.py
@@ -0,0 +1,19 @@
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        self.diameter = 0
+
+        def dfs(node):
+            if not node:
+                return 0
+
+            left_height = dfs(node.left)
+            right_height = dfs(node.right)
+
+            # Longest path passing through this node
+            self.diameter = max(self.diameter, left_height + right_height)
+
+            # Return this subtree's height to the parent
+            return 1 + max(left_height, right_height)
+
+        dfs(root)
+        return self.diameter
