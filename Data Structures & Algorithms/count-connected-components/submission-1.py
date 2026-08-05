580543b72720a13439acc43327eea6bea084ae68
timeflyer2005 <yanjerry2015@gmail.com>
2026-08-05 10:47:14 -0400

    Add: count-connected-components - submission-1

---
+++ Data Structures & Algorithms/count-connected-components/submission-1.py
@@ -0,0 +1,34 @@
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+ 
+        graph = {i: [] for i in range(n)}
+        for n1, n2 in edges:
+            graph[n1].append(n2)
+            graph[n2].append(n1)
+
+        
+        count = 0
+        visit = set()
+        
+        def dfs(node):
+            if node in visit:
+                return
+            visit.add(node)
+
+            for neighbor in graph[node]:
+                dfs(neighbor)
+            
+        for node in range(n):
+            if node not in visit:
+                dfs(node)
+                count += 1
+        
+        return count 
+
+
+            
+            
+            
+
+
+
